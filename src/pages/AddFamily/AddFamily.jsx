1d7c6deeadcfb038263d20789d7bd7b0d264884f
Chris Campbell <christopherecampbell@gmail.com>
2023-09-18 13:11:24 -0600

    add family search

--- src/pages/AddFamily/AddFamily.jsx
+++ src/pages/AddFamily/AddFamily.jsx
@@ -13,7 +13,32 @@ import styles from './AddFamily.module.css'
 const AddFamily = ({ user, profile }) => {
   const [profiles, setProfiles] = useState([])
 
-  console.log(profile)
+  console.log(profiles)
+
+  // search form state
+  const [formData, setFormData] = useState({
+    search: '',
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSearch = async (e) => {
+    e.preventDefault()
+    if (formData.search !== '') {
+      const nameFilter = profiles.filter((el) => el.name.toLowerCase().includes(formData.search.toLowerCase()))
+      // const emailFilter = profiles.filter((el) => el.email.toLowerCase().includes(formData.search.toLowerCase()))
+      // const filtered = [...nameFilter, ...emailFilter]
+      // const uniqueFilter = [... new Set(filtered)]
+      // console.log(filtered)
+      setProfiles(nameFilter)
+      setFormData({ search: '' })
+    }
+
+  }
+
+  // console.log(profile)
   useEffect(() => {
     const fetchProfiles = async () => {
       const profileData = await profileService.getAllProfiles()
@@ -30,7 +55,31 @@ const AddFamily = ({ user, profile }) => {
   
   return (
     <main className={styles.container}>
-      <h1>Hello. This is a list of all the profiles.</h1>
+      <h1>Send A Request to Add Family Members</h1>
+      <form
+      className={styles.container}
+      autoComplete="off"
+      onSubmit={handleSearch}
+    >
+      <div className={styles.inputContainer}>
+        {/* <label htmlFor="search">Name</label> */}
+        <input
+          type="text"
+          name="search"
+          placeholder='search by name'
+          id="search"
+          value={formData.search}
+          onChange={handleChange}
+        />
+      </div>      
+      <button
+        // onClick={handleSubmit}
+      >
+        Search
+      </button>
+
+      </form>
+
       {profiles.map(profileDisplayed => (
         // <p key={profile._id}>{profile.name}</p>
         <ProfileCard key={profileDisplayed._id} profileDisplayed={profileDisplayed} profile={profile}/>
