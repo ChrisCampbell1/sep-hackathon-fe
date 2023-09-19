306e808403ff2526ff92243370f562382890e2bd
Chris Campbell <christopherecampbell@gmail.com>
2023-09-18 23:14:55 -0600

    style media form

--- src/components/MediaUploadForm/MediaUploadForm.jsx
+++ src/components/MediaUploadForm/MediaUploadForm.jsx
@@ -11,19 +11,20 @@ import * as recipeService from '../../services/recipeService'
 
 // styles
 import styles from './MediaUploadForm.module.css'
+import icon from '../../assets/recipes-icon.png'
 
 // component
 
 
 export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber, profile, recipes, setRecipes, setRecipe, recipe }) {
   const navigate = useNavigate()
-  
+
   // holds the media that will be sent via the recipe service to the back end when the user hits next or back
   const [image, setImage] = useState(null)
   const [video, setVideo] = useState(null)
   const [audio, setAudio] = useState(null)
   const [formData, setFormData] = useState(
-    {share: false}
+    { share: false }
   )
 
   // updates public based on checkbox
@@ -31,7 +32,7 @@ export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber
     console.log("check")
     setFormData((prevValue) => {
       console.log(prevValue)
-      return {share: !prevValue.share}
+      return { share: !prevValue.share }
     }
     )
   }
@@ -92,13 +93,16 @@ export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber
   }
 
 
-    return (
-      <form className={styles.container}>
+  return (
+    <form className={styles.container}>
+      <label>Media</label>
+      <div className={styles.inputContainer}>
+        <p>Help your dish shine! Here's your chance to add pictures, video, or audio about your special dish!</p>
         <div className={styles.inputContainerUpload}>
           {image ?
-            <label htmlFor="image">Image Selected</label>
+            <label htmlFor="image" className={styles.selected}>Image Selected</label>
             :
-            <label htmlFor="image">Select Image</label>
+            <label htmlFor="image">+ Select Image</label>
           }
 
           <input
@@ -111,9 +115,9 @@ export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber
         </div>
         <div className={styles.inputContainerUpload}>
           {video ?
-            <label htmlFor="video">Video Selected</label>
+            <label htmlFor="video" className={styles.selected}>Video Selected</label>
             :
-            <label htmlFor="video">Select Video</label>
+            <label htmlFor="video" >+ Select Video</label>
           }
 
           <input
@@ -127,9 +131,9 @@ export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber
         </div>
         <div className={styles.inputContainerUpload}>
           {audio ?
-            <label htmlFor="audio">Audio Selected</label>
+            <label htmlFor="audio" className={styles.selected}>Audio Selected</label>
             :
-            <label htmlFor="audio">Select Audio</label>
+            <label htmlFor="audio">+ Select Audio</label>
           }
 
           <input
@@ -141,35 +145,47 @@ export default function MediaUploadForm({ increaseFormNumber, decreaseFormNumber
             required
           />
         </div>
-        <div className={styles.inputContainerCheck}>
-        <label htmlFor="share">Share Publicly?</label>
-        <input
-          type="checkbox"
-          name="share"
-          id="share"
-          onChange={handleCheckboxChange}
-        />
-      </div>
+        <p>Examples</p>
+          <li>Add a picture of your dish when finished.</li>
+          <li>Add a picture of the original hand-written recipe to help preserve it.</li>
+          <li>Add a video of you cooking the dish.</li>
+          <li>Add an audio recording of you retelling the family history of the dish and how it has been passed down.</li>
+          </div>
+          <label>Sharing</label>
+          <div className={styles.inputContainer}>
+            <p>You're done making your recipe. The final step is to choose if you want to share this recipe with the world.</p>
+            <div className={styles.inputContainerCheck}>
+              <label htmlFor="share">
+                <img src={icon} alt="icon of recipe cards" />
+                Make Recipe Public</label>
+              <input
+                type="checkbox"
+                name="share"
+                id="share"
+                onChange={handleCheckboxChange}
+              />
+            </div>
+          </div>
+
 
       <div className={styles.bottom}>
-          <p></p>
-          <div className={styles.buttons}>
-
-          </div>
+        <p>When you're done adding media and sharing permissions for your dish use the continue button to finish the process of adding your recipe.</p>
+        <div className={styles.buttons}>
+          <button
+            type='button'
+            onClick={handleBack}
+          >
+            Upload and Back
+          </button>
+          <button
+            type='button'
+            onClick={handleSubmit}
+          >
+            Upload and Save
+          </button>
         </div>
+      </div>
 
-        <button
-          type='button'
-          onClick={handleSubmit}
-        >
-          Upload and Save
-        </button>
-        <button
-          type='button'
-          onClick={handleBack}
-        >
-          Upload and Back
-        </button>
-      </form>
-    )
-  }
+    </form>
+  )
+}
